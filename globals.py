4e23dbe4ec5836078f54563b133ae8f9e48f28e7
Kacper Karpiński <32614349+Kacperocik@users.noreply.github.com>
2026-05-17 13:37:58 +0200

    Add contstants to globals containing database directory name and files paths (#2)

---
+++ globals.py
@@ -0,0 +1,10 @@
+import os
+
+
+DB_DIR = 'DATABASE'
+PROD_FILE = os.path.join(DB_DIR, 'products.xlsx')
+CUST_FILE = os.path.join(DB_DIR, 'customer.csv')
+ADDR_FILE = os.path.join(DB_DIR, 'address.csv')
+SETT_FILE = os.path.join(DB_DIR, 'settings.csv')
+LANG_FILE = os.path.join(DB_DIR, 'translations.csv')
+LOGO_FILE = os.path.join(DB_DIR, 'logo.svg')
